6554788e7f50ed9aa48e6fbb720a2f2ef80a0aaf
naomijustin <naomi.z.justin@gmail.com>
2023-05-21 15:56:58 +1000

    Navbar styling updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from "react"
+import logo from "../assets/react.svg"
+
+/**
+Challenge: Complete the Navbar to match the design
+
+Hint: use the Figma file for the most accurate peek at the design
+(colors, sizes, fonts, etc.)
+*/
 
 function Navbar() {
     return (
-        <h1>Navbar goes here</h1>
+        <nav className="navbar">
+            <div className="navFirst">
+                <img src={logo}></img>
+                <h1>ReactFacts</h1>
+            </div>            
+            <h2>React Course - Project 1</h2>
+        </nav>
+        
     )
 }
 
