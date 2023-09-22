21707b7c9b90f0544cebc3c047bbb22e0faec29a
naomijustin <naomi.z.justin@gmail.com>
2023-09-22 17:10:54 +1000

    added code from Scrimba for challenge

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,35 +2,17 @@ import React from "react"
 import logo from "../assets/react.svg"
 import logoLarge from "../assets/reactjs-icon_large.svg"
 
-
-/**
-Challenge: Build the main section!
-
-Skip 2 aspects of the design for now:
-1. The colored bullets in the list
-2. The larger React logo on the side
-
-Those will be separate challenges coming up.
-*/
-
-
-
-
-function Main() {
+export default function Main(props) {
     return (
-        <main className="main--container">
+        <main className={props.darkMode ? "dark" : ""}>
             <h1 className="main--title">Fun facts about React</h1>
-            <ul className="main--ul">
+            <ul className="main--facts">
                 <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li> 
-                <li>Has well over 100K stars on GitHub</li> 
-                <li>Is maintained by Facebook</li> 
-                <li>Powers thousands of enterprise apps, including mobile apps</li> 
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-            <img className="main--bg_logo" src={logoLarge} alt="React Logo"></img>
         </main>
-        
     )
 }
-
-export default Main
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,45 @@
 import React from "react"
 import logo from "../assets/react.svg"
 
-function Navbar() {
-    return (
-        <nav className="navbar">
-            <div className="navFirst">
-                <img className="nav--logo" src={logo}></img>
-                <h3 className="nav--logo_text">ReactFacts</h3>
-            </div>            
-            <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
+// function Navbar() {
+//     return (
+//         <nav className="navbar">
+//             <div className="navFirst">
+//                 <img className="nav--logo" src={logo}></img>
+//                 <h3 className="nav--logo_text">ReactFacts</h3>
+//             </div>            
+//             <h4 className="nav--title">React Course - Project 1</h4>
+//         </nav>
         
+//     )
+// }
+
+// export default Navbar
+
+
+export default function Navbar(props) {
+    return (
+         <nav 
+            className={props.darkMode ? "dark": ""}
+        >
+            <img 
+                className="nav--logo_icon"
+                src="./images/react-icon-small.png"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            
+            <div 
+                className="toggler" 
+            >
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider"
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
+        </nav>
     )
 }
-
-export default Navbar
