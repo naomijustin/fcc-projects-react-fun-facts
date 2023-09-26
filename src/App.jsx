278dbe281cce4d4bdf2444bb35a42b85957644d9
naomijustin <naomi.z.justin@gmail.com>
2023-09-26 17:03:48 +1000

    dark mode toggle working on nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 function App() {
+  
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  const handleClick = function() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+    console.log(darkMode);
+  }
+
+
+
   return (
     <div className="container">
-      <Navbar />
+      <Navbar darkMode={darkMode} toggleDarkMode={handleClick} />
       <Main />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,13 @@
 import React from "react"
 import logo from "../assets/react.svg"
 
-// function Navbar() {
-//     return (
-//         <nav className="navbar">
-//             <div className="navFirst">
-//                 <img className="nav--logo" src={logo}></img>
-//                 <h3 className="nav--logo_text">ReactFacts</h3>
-//             </div>            
-//             <h4 className="nav--title">React Course - Project 1</h4>
-//         </nav>
-        
-//     )
-// }
-
-// export default Navbar
-
-
 export default function Navbar(props) {
+    console.log(props)
     return (
-         <nav 
-            className={props.darkMode ? "dark": ""}
-        >
-            <img 
-                className="nav--logo_icon"
-                src={logo}
-            />
+         <nav className={props.darkMode ? "dark": ""}>
+            <img className="nav--logo_icon" src={logo} />
             <h3 className="nav--logo_text">ReactFacts</h3>
-            
-            <div 
-                className="toggler" 
-            >
+            <div className="toggler">
                 <p className="toggler--light">Light</p>
                 <div 
                     className="toggler--slider"
